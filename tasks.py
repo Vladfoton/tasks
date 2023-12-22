7f4315227af1e43eafb51969492f6def1429caeb
Vladfoton <vladkuznetcov@mail.ru>
2023-12-22 20:29:39 +0700

    решение варианта 15

--- tasks.py
+++ tasks.py
@@ -10,7 +10,7 @@ def type2():
         for y in range(2):
             for z in range(2):
                 for w in range(2):
-                    if not ( (x == (not(y))) <= ((x and w) == (z and (not(w))))):
+                    if not ((x == (not (y))) <= ((x and w) == (z and (not (w))))):
                         print(x, y, z, w)
 
 
@@ -22,9 +22,9 @@ def type5():
         count1 = nb.count('1')
         count0 = nb.count('0')
         if count1 > count0:
-            nb+='1'
+            nb += '1'
         else:
-            nb+='0'
+            nb += '0'
         return nb
 
     print(int(fff(13), 2))
@@ -44,7 +44,6 @@ def type6():
     for _ in range(5):
         turtle.forward(9 * n)
         turtle.right(120)
-
 
     turtle.up()
 
@@ -66,7 +65,7 @@ def type8():
     dd = product(s, repeat=5)
     dd = sorted(list(dd))
     print(*dd)
-    for i, c in enumerate(dd, start =1):
+    for i, c in enumerate(dd, start=1):
         print(i, c)
 
 
@@ -104,7 +103,7 @@ def type15():
     for a in range(1000):
         flag = True
         for x in range(1000):
-            if not (((x%3 ==0) <= (not(x%5 ==0))) or (x+a>=90)):
+            if not (((x % 3 == 0) <= (not (x % 5 == 0))) or (x + a >= 90)):
                 flag = False
                 break
         if flag:
@@ -127,19 +126,21 @@ def type17():
         num_data = [i[:-1] for i in f.readlines()]
         print(num_data)
         count = 0
-        maxsum =0
+        maxsum = 0
         filtered_num = [int(i) for i in num_data if (len(i) == 3 and i[-1] == '5')]
         delitel = min(filtered_num)
         print(f'{delitel=}')
         result = []
         for i in range(len(num_data) - 1):
-            if (((len(num_data[i]) ==4) or (len(num_data[i+1]) == 4)) and (len(num_data[i]) + len(num_data[i+1]) !=8)):
-                summq = int(num_data[i]) **2 + int(num_data[i+1]) ** 2
-                if summq%delitel == 0:
-                    count+=1
-                    maxsum = max (maxsum, summq)
+            if (((len(num_data[i]) == 4) or (len(num_data[i + 1]) == 4)) and (
+                    len(num_data[i]) + len(num_data[i + 1]) != 8)):
+                summq = int(num_data[i]) ** 2 + int(num_data[i + 1]) ** 2
+                if summq % delitel == 0:
+                    count += 1
+                    maxsum = max(maxsum, summq)
 
         print(f'{count=}  {maxsum=}')
+
 
 def type23():
     def f(start, end):
@@ -150,44 +151,43 @@ def type23():
         elif start > end:
             return 0
         else:
-            return f(start+2, end) + f(start*2, end)
+            return f(start + 2, end) + f(start * 2, end)
 
-    print(f(1,4) * f(4,8))
+    print(f(1, 4) * f(4, 8))
 
 
 def type24():
     with open('1_24.txt', 'r') as f:
-        minlen=10000000000000000000000000
+        minlen = 10000000000000000000000000
         str_data = f.readline()
-        str_data= str_data.replace('TT', "*")
+        str_data = str_data.replace('TT', "*")
         print(str_data)
         print(str_data.count('*'))
-        for start in range(0,len(str_data)-1):
+        for start in range(0, len(str_data) - 1):
             countsimbols = 0
-            count_tt =0
+            count_tt = 0
             for c in range(start, len(str_data)):
                 if str_data[c] == "*":
                     count_tt += 1
-                countsimbols+=1
+                countsimbols += 1
                 if count_tt == 150:
-                    minlen = min(minlen,countsimbols+150)
+                    minlen = min(minlen, countsimbols + 150)
                     print(f'{start=} Current_len = {minlen}')
                     countsimbols = 0
                     count_tt = 0
 
-
-
-
         print(minlen)
+
+
 def type25():
     def delit(n):
-        count =0
-        delit_list=[]
-        for i in range(2, int(n/2),2):
-            if n%i ==0:
+        count = 0
+        delit_list = []
+        for i in range(2, int(n / 2), 2):
+            if n % i == 0:
                 count += 1
                 delit_list.append(i)
-            if count>3:
+            if count > 3:
                 return False
         if count == 3:
             print(f'{n=}, {delit_list=}')
@@ -205,9 +205,9 @@ def type25():
 
 def type26():
     with open('26.txt', 'r') as f:
-        rez={}
+        rez = {}
         list_data = [i[:-1] for i in f.readlines()]
-        nums, summa =map(int, list_data.pop(0).split())
+        nums, summa = map(int, list_data.pop(0).split())
         print(nums, summa)
         for string in list_data:
             cost, type_izd = string.split()
@@ -217,12 +217,12 @@ def type26():
                 rez[type_izd] = [int(cost)]
         for key in rez:
             print(sorted(rez[key]))
-        count_a=0
-        count_summa =0
+        count_a = 0
+        count_summa = 0
         for aa in sorted(rez['A']):
-            if count_summa + int(aa)<=summa:
-                count_a+=1
-                count_summa+=int(aa)
+            if count_summa + int(aa) <= summa:
+                count_a += 1
+                count_summa += int(aa)
             else:
                 break
         for bb in sorted(rez['B']):
@@ -231,44 +231,63 @@ def type26():
             else:
                 print(f'{count_a=}  delta = {summa - count_summa}')
 
+
 def type27(filename):
+    def count_odd_even(datalist) -> int:
+        # считаем количество четных и нечетных
+        # если четных больше возвращаем 0 иначе 1
+        # если равны то 2
+        count_1 = 0
+        count_0 = 0
+        for i in datalist:
+            if i % 2:
+                count_1 += 1
+            else:
+                count_0 += 1
+        if count_0 == count_1:
+            return 2
+        if count_1 > count_0:
+            return 1
+        else:
+            return 0
 
     with open(filename, 'r') as f:
         string_data = f.readlines()
-        string_data.pop(0)
-        string_data = [[int(j) for j in i.split()] for i in string_data]
-        filter_data1 = list(filter(lambda x: x[0] % 2 == 1, string_data))
-        maxsum = 0
-        for i in range(len(filter_data1)):
-            filter_data = copy.deepcopy(filter_data1)
-            summlist = [i[0] + i[1] for i in filter_data]
-            param = [(i[0] % 2, i[1] % 2) for i in filter_data]
-            summore = sum([max(i[0],i[1]) for i in filter_data])
-            sumless = sum([min(i[0],i[1]) for i in filter_data])
-            if summore%2 == 1 and sumless%2 == 0:
-                if maxsum < summore + sumless:
-                    maxsum = summore + sumless
+        len_list = int(string_data.pop(0))
+        print(len_list)
+        list_min = []
+        list_max = []
 
-        print(maxsum)
+        for data in string_data:
+            list_min.append(min(map(int, data.split())))
+            list_max.append(max(map(int, data.split())))
+        listmin = sorted(list_min, reverse=True)
+        listmax = sorted(list_max, reverse=True)
+        rezult = copy.deepcopy(listmax)
+
+
 
 
 def newyear_chalenge1():
-    string ='ЗЖ АЮДЮЖФВ, АДЗВ Б ЗРЮЖХ ЖЮ ДЧЪБЛ ЙЗЯЭЮКЛЫЗ'
+    string = 'ЗЖ АЮДЮЖФВ, АДЗВ Б ЗРЮЖХ ЖЮ ДЧЪБЛ ЙЗЯЭЮКЛЫЗ'
     alphabeet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ'
 
     for i in range(33):
         str1 = ''
         for char in string:
-            str1+= chr(ord(char)+i)
+            str1 += chr(ord(char) + i)
         print(f'{i=} : {str1}')
+
+
 def newyear_chalenge4():
-    rezult=[]
-    num=1
-    for i in range(1,101):
-        rezult.append((num, num+1, num+2))
-        num+=3
+    rezult = []
+    num = 1
+    for i in range(1, 101):
+        rezult.append((num, num + 1, num + 2))
+        num += 3
     print(rezult)
     print(sum(rezult[99]))
+
 
 def newyear_chalenge5():
     from collections import deque
@@ -291,5 +310,45 @@ def newyear_chalenge5():
     dd.append('!')
     print(dd)
 
+
+class BreakLoops(Exception):
+    pass
+def newyear_chalenge10():
+    try:
+        for a in range(27, 201):
+            print(f'{a=}')
+            for b in range(a+1, 201):
+                for c in range(b+1, 201):
+                    for d in range(c+1, 201):
+                        for e in range(d+1, 201):
+                            if ((a ** 5 + b ** 5 + c ** 5 + d ** 5) == e ** 5):
+                                print(a, b, c, d, e)
+                                raise BreakLoops
+    except BreakLoops:
+        pass
+
 if __name__ == '__main__':
-    type25()
+    data = [4,
+            8,
+            15,
+            16,
+            3,
+            14,
+            15,
+            192,
+            168,
+            1992,
+            37,
+            256,
+            98,
+            101,
+            273,
+            7787,
+            918,
+            39,
+            1098,
+            222,
+            69]
+    print(len(data))
+
+
